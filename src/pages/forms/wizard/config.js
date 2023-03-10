fea80bbac826c77e0fe5ab1a1f014752d2945250
hyojeong_kim <hyojeong_kim@miraeasset.com>
2023-03-10 16:12:07 +0900

    change to 5001

--- src/pages/forms/wizard/config.js
+++ src/pages/forms/wizard/config.js
@@ -14,7 +14,7 @@ const chartColors = {
 };
 global.XMLHttpRequest = require("xhr2");
 
-const url = "http://172.16.120.19:5000";
+const url = "http://172.16.120.19:5001";
 //const url = "http://43.200.170.131:5000";
 export function httpGet(theURL) {
   const xmlHttp = new XMLHttpRequest();
